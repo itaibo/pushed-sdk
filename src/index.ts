29b47df0fe42560adeb42d3d887de9a03e3061d7
Iñigo Taibo <itaibo@hey.com>
2022-11-28 16:42:58 +0100

    Pushed app

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,52 @@
 import { default as requestHelper, RequestParameters } from './helpers/request.js';
 
-export default class Package {
-	apiKey: string;
+const PUSHED_API_BASE_URL = 'https://api.pushed.co/1';
+
+export interface Initializer {
+	app_key: string;
+	app_secret: string;
+};
+
+export interface NotificationRequest {
+	content: string;
+	content_type?: 'url';
+	content_extra?: string;
+	target_type: 'app' | 'channel' | 'user' | 'pusher_id' | 'subscription_alias' | 'email';
+	target_alias?: string;
+	pushed_id?: string;
+	email?: string;
+};
+
+export default class PushedSdk {
+	app_key: string;
+	app_secret: string;
 	headers: object;
 
-	constructor(apiKey: string) {
-		this.apiKey = apiKey;
-		this.headers = {
-			'Authorization': `Bearer ${this.apiKey}`,
-			'Content-Type': 'application/json',
-		};
+	constructor(initializer: Initializer) {
+		this.app_key = initializer.app_key;
+		this.app_secret = initializer.app_secret;
 	}
 
 	// Utils
 	async request(path: string = '', parameters: RequestParameters = {}) {
+		const data = {
+			...(parameters?.data),
+			app_key: this.app_key,
+			app_secret: this.app_secret,
+		};
+
 		return await requestHelper({
 			method: parameters?.method || 'get',
-			url: `${path}`,
-			headers: this.headers,
-			...parameters,
+			url: `${PUSHED_API_BASE_URL}${path}`,
+			data,
 		});
 	}
 
 	// Scope
-	public readonly scope1 = {
-		create: async (parameters: object = {}): Promise<any> => {
-			const result = await this.request('/scope1', { method: 'post', data: parameters });
-			return result?.server || result;
+	public readonly notifications = {
+		send: async (parameters: object = {}): Promise<any> => {
+			const result = await this.request('/push', { method: 'post', data: parameters });
+			return result;
 		},
 	};
 };
